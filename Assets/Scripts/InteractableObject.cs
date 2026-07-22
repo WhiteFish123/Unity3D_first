029c241f29dfa5220fecd6882513b1131b2bfd9a
WhiteFish123 <bohe03554@gmail.com>
2026-07-22 14:31:24 +0800

    调整了交互的时机，防止点击交互对象的时候，还没走到目的地就直接交互的情况， 现在只有走到交互对象跟前才会开启交互 重构：原逻辑是判断是否路径计算完成，并且剩余路径是否小于等于停止距离，用了嵌套的if-else 结构，专门弄了一个bool变量防止在Update框架中频繁调用 现在改良为用协程实现，无需调用Update框架，等到协程的最后一步才会正式交互，中间如果出现新的协程顶替原协程的情况，不会导致最后交互异常

--- Assets/Scripts/InteractableObject.cs
+++ Assets/Scripts/InteractableObject.cs
@@ -1,16 +1,44 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
 public class InteractableObject : MonoBehaviour
 {
+    private NavMeshAgent playerAgent;
+    bool isInteracting=false;
+    private Coroutine moveToObjectCo;//先创建一个协程变量
     public void OnClick(NavMeshAgent playerAgent)
     {
-        playerAgent.stoppingDistance=2;
-        playerAgent.SetDestination(transform.position);  
+        this.playerAgent=playerAgent;
+        if(moveToObjectCo!=null)//如果协程不为空，说明之前已经有一个协程在执行了
+        {
+            StopCoroutine(moveToObjectCo);//那么就先停止之前的协程
+        }
+        moveToObjectCo=StartCoroutine(MoveToObjectCo());
+        //开启新的协程，MoveToObjectCo是方法，moveToObjectCo是协程变量
+        //协程变量的作用是为了在下一次点击时，能够停止之前的协程，避免多个协程同时执行
 
-        Interact();
+        //Interact();
     }
+    
     protected virtual void Interact()
     {
         Debug.Log("Interacting with " + gameObject.name);
+    }
+    private IEnumerator MoveToObjectCo()
+    {
+        //检查playerAgent是否为空
+        //检查路径是否可达
+        if (playerAgent == null) yield break;
+        playerAgent.stoppingDistance=2;
+        if (!playerAgent.SetDestination(transform.position))//设置目的地的同时检查是否可达
+        {
+            Debug.LogWarning("Destination unreachable");
+            yield break;
+        }
+
+        yield return new WaitUntil(()=>playerAgent.pathPending==false);//先等待计算完路径
+        yield return new WaitUntil(()=>playerAgent.remainingDistance<=playerAgent.stoppingDistance);//再等待玩家到达目的地
+
+        Interact();//正式交互
     }
 }
