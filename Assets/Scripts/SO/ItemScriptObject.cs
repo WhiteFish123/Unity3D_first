3655c63dbafc26aae5a66331344c8c1890eb751a
WhiteFish123 <bohe03554@gmail.com>
2026-07-24 14:19:14 +0800

    通过ScriptObject和Enum实现物体数据存储，实现了物体通用的基类脚本

---
+++ Assets/Scripts/SO/ItemScriptObject.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+[CreateAssetMenu]
+public class ItemScriptObject : ScriptableObject
+{
+    public string itemName;
+    public ItemType itemType;
+    public string description;
+    public List<ItemProperty> itemProperties;
+    public Sprite itemIcon;
+    public GameObject itemPrefab;
+}
+public enum ItemType
+{
+    Weapon,
+    Consumable
+}
+[Serializable]
+public class ItemProperty
+{
+    public ItemPropertyType propertyName;
+    public int propertyValue;
+}
+public enum ItemPropertyType
+{
+    HPValue,
+    EnergyValue,
+    MentalValue,
+    SpeedValue,
+    AttackValue,
+}
